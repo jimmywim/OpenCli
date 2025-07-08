77db82420d1d63eb45ffb4e688fb20d25c07bd38
Jim Love <james@id-live.com>
2025-07-08 13:29:27 +0100

    Clarified flow of sample, added extra explanatory comments

--- OpenCli.CommandLine.Sample/Program.cs
+++ OpenCli.CommandLine.Sample/Program.cs
@@ -5,17 +5,23 @@ using System.Reflection;
 
 namespace OpenCli.CommandLine.Sample
 {
+    /// <summary>
+    /// A sample application that uses CommandLineParser to define the possible arguments
+    /// Generates a OpenCliDocument class instance, and also creates a JSON document from the generated class.
+    /// </summary>
     internal class Program
     {
         static async Task Main(string[] args)
         {
             // Generate a OCS document
-
             var openCli = OpenCliDocumentGenerator.CreateFromAssembly(Assembly.GetExecutingAssembly());
 
             using IOpenCliDocumentSerializer jsonSerializer = new OpenCliDocumentJsonSerializer(openCli);
             var json = await jsonSerializer.SerializeAsync();
 
+
+            // Run the actual app with the provided command line arguments, parsed with CommandLineParser
+            // and available as type-safe properties of the SampleOptions class
             Parser.Default.ParseArguments<SampleOptions>(args)
                 .WithParsed<SampleOptions>(o =>
                 {
--- OpenCli.CommandLine.Sample/SampleOptions.cs
+++ OpenCli.CommandLine.Sample/SampleOptions.cs
@@ -2,6 +2,10 @@
 
 namespace OpenCli.CommandLine.Sample
 {
+    /// <summary>
+    /// A sample of the CommandLineParser options definition
+    /// More examples here: https://github.com/commandlineparser/commandline/tree/master?tab=readme-ov-file#c-examples
+    /// </summary>
     internal class SampleOptions
     {
         [Option('t', "TestOption", Required = false, HelpText = "This is a sample option")]
